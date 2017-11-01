c90415ee22a34d06d2856113ebf7bc8974563b2e
Jared George <jaredgeorge.us@gmail.com>
2017-11-01 15:17:11 -0700

    add confirm display for user-account

--- static/js/user-account.js
+++ static/js/user-account.js
@@ -1,15 +1,75 @@
 $(document).ready(function() {
 
+
+// -- JS for vendor confirm display --//
+$("#venueButton").click(function(){
+    $('#venueStatus').toggleClass('is-selected')
+    let x = $("#venueButton")
+    if (x.text() == "Confirm") {
+        x.text('Cancel')
+    } else {
+        x.text('Confirm')
+    }
+})
+
 $("#photoButton").click(function(){
     $('#photoStatus').toggleClass('is-selected')
     let x = $("#photoButton")
-    if (x.text() == "Find") {
-        x.text('Confirm')
+    if (x.text() == "Confirm") {
+        x.text('Cancel')
     } else {
-        x.text('Find')
+        x.text('Confirm')
     }
-
-
-
 })
+
+$("#videoButton").click(function(){
+    $('#videoStatus').toggleClass('is-selected')
+    let x = $("#videoButton")
+    if (x.text() == "Confirm") {
+        x.text('Cancel')
+    } else {
+        x.text('Confirm')
+    }
+})
+
+$("#catererButton").click(function(){
+    $('#catererStatus').toggleClass('is-selected')
+    let x = $("#catererButton")
+    if (x.text() == "Confirm") {
+        x.text('Cancel')
+    } else {
+        x.text('Confirm')
+    }
+})
+
+$("#bandButton").click(function(){
+    $('#bandStatus').toggleClass('is-selected')
+    let x = $("#bandButton")
+    if (x.text() == "Confirm") {
+        x.text('Cancel')
+    } else {
+        x.text('Confirm')
+    }
+})
+
+$("#hairButton").click(function(){
+    $('#hairStatus').toggleClass('is-selected')
+    let x = $("#hairButton")
+    if (x.text() == "Confirm") {
+        x.text('Cancel')
+    } else {
+        x.text('Confirm')
+    }
+})
+
+$("#tailorButton").click(function(){
+    $('#tailorStatus').toggleClass('is-selected')
+    let x = $("#tailorButton")
+    if (x.text() == "Confirm") {
+        x.text('Cancel')
+    } else {
+        x.text('Confirm')
+    }
+})
+
 })
