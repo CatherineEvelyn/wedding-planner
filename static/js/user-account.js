1a3db10ee8b5fdc4f6c6655208bc0b6b5d4bcc22
CatherineEvelyn <catherine_jr2006@hotmail.com>
2017-11-06 14:04:35 -0800

    fix Modal bug on user account page

--- static/js/user-account.js
+++ static/js/user-account.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
 
 var globalModal = $('.global-modal');
-    $( ".btn-green-flat-trigger" ).on( "click", function(e) {
-      e.preventDefault();
-      $( globalModal ).toggleClass('global-modal-show');
-    });
     $( ".overlay" ).on( "click", function() {
       $( globalModal ).toggleClass('global-modal-show');
     });
@@ -13,6 +9,42 @@ var globalModal = $('.global-modal');
     });
     $(".mobile-close").on("click", function(){
       $( globalModal ).toggleClass('global-modal-show');
+    });
+
+//To call specific Modal to open when a specific button is clicked
+    $( "#venueButton" ).on( "click", function(e) {
+      e.preventDefault();
+      $( venueModal ).toggleClass('global-modal-show');
+    });
+
+    $( "#photoButton" ).on( "click", function(e) {
+      e.preventDefault();
+      $( photoModal ).toggleClass('global-modal-show');
+    });
+
+    $( "#videoButton" ).on( "click", function(e) {
+      e.preventDefault();
+      $( videoModal ).toggleClass('global-modal-show');
+    });    
+
+    $( "#catererButton" ).on( "click", function(e) {
+      e.preventDefault();
+      $( catererModal ).toggleClass('global-modal-show');
+    });
+
+    $( "#musicButton" ).on( "click", function(e) {
+      e.preventDefault();
+      $( musicModal ).toggleClass('global-modal-show');
+    });
+
+    $( "#hairButton" ).on( "click", function(e) {
+      e.preventDefault();
+      $( cosmeticsModal ).toggleClass('global-modal-show');
+    });
+
+    $( "#tailorButton" ).on( "click", function(e) {
+      e.preventDefault();
+      $( tailorModal ).toggleClass('global-modal-show');
     });
 
 })
