4ad629b43e738ac43627e1aa55df717fba590efe
Sean Stevens <sstevens88@gmail.com>
2017-11-17 18:57:43 -0800

    changed asset structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const package = require('./package.json');
 
 module.exports = {
   entry: {
-    main: "./assets"
+    main: "./src/js/main.js",
+    vendor: Object.keys(package.dependencies),
+    bookingViewer: "./src/js/bookingViewer.js",
+    datepicker: "./src/js/datepicker.js",
+    vendorProfile: "./src/js/vendor-profile.js",
+    userAccount: "./src/js/user-account.js"
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'static')
+  },
+  watch: true,
+  resolve: { extensions: [".js"] },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "My Wedding Planner",
+      my
+    })
+  ]
   module: {
     loaders: [
       {
@@ -16,8 +36,4 @@ module.exports = {
       }
     ]
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'static')
-  }
 };
